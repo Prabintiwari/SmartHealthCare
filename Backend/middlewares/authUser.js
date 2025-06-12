62dd1067b27fdd829d6e3dfc8d6085d4e5f94000
unknown <prabintiwari964@gmail.com>
2025-06-12 09:51:30 +0200

    Save local changes before pull

---
+++ Backend/middlewares/authUser.js
@@ -0,0 +1,25 @@
+import jwt from "jsonwebtoken";
+
+//User authentication middleware
+
+const authUser = async (req, res, next) => {
+  try {
+
+      const {token} = req.headers
+      if (!token) {
+        return res.json({ success: false, message: "Access Denied" });
+        
+      }
+      const tokenDecode = jwt.verify(token, process.env.JWT_SECRET);
+      req.body.userId = tokenDecode.id;
+      next();
+    
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export default authUser;
+
+
